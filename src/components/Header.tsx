4b315940cbf983f56f7fcb9c44c52c8d2433e90b
nizar <niz.benlahser@gmail.com>
2026-01-05 15:43:58 +0100

    Change Header Image size in CSS

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -242,14 +242,14 @@ export const Header: React.FC = () => {
             <Image
               src={AGENCY_INFO.logoLight}
               alt={`${AGENCY_INFO.subtitle} Logo`}
-              className="h-10 md:h-14 w-auto object-contain block dark:hidden"
+              className="h-30 md:h-32 w-30 object-contain block dark:hidden"
               width={120}
               height={120}
             />
             <Image
               src={AGENCY_INFO.logoDark}
               alt={`${AGENCY_INFO.subtitle} Logo Dark`}
-              className="h-10 md:h-14 w-auto object-contain hidden dark:block"
+              className="h-30 md:h-32 w-30 object-contain hidden dark:block"
               width={120}
               height={120}
             />
